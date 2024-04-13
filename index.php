dc16a19d1aa843d3d15a5813d482a75cb5db998a
vochilong <vochilong.work@gmail.com>
2024-04-13 17:16:46 +0700

    :tada: send first message chatwork

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<h1>Hello auto CICD github action</h1>
-<p>My first package github action</p>
-<p>Something done !! v1 kakaka PHP</p>
 <?php
 $token = "379d91af33d874e35b8d3a47c646a5de";
 $payload = file_get_contents('php://input');
@@ -30,7 +27,7 @@ if ($result['code'] == 200) {
     echo "Có lỗi xảy ra khi gửi tin nhắn.";
 }
 
-function sendMessage($data, $url, $token)
+function sendMessage($data, $url, $token): array
 {
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -60,10 +57,29 @@ function sendMessage($data, $url, $token)
 
 function writeToLog($message)
 {
-    $logFile = "logs.txt";
-    $fileHandle = fopen($logFile, 'a') or die("Không thể mở tệp log.");
-    $logLine = date('Y-m-d H:i:s') . " - " . $message . "\n";
-    fwrite($fileHandle, $logLine);
-    fclose($fileHandle);
+    try {
+        $logFile = __DIR__ . "/logs.txt";
+        $logLine = date('Y-m-d H:i:s') . " - " . $message . "\n";
+        file_put_contents($logFile, $logLine, FILE_APPEND | LOCK_EX);
+        var_dump(file_put_contents($logFile, $logLine, FILE_APPEND | LOCK_EX));
+        die();
+    } catch (\Throwable $throwable) {
+        // TODO: Handle error where write logs!
+    }
 }
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Webhook Chatwork</title>
+</head>
+<body>
+    <center><h1>Welcome to send message Chatwork</h1></center>
+</body>
+</html>
 
