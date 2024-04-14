10b4d7f45a0e18cfa94b3cfc17e9be978a395291
vochilong <vochilong.work@gmail.com>
2024-04-14 22:29:59 +0700

    :tada: handle reply webhook

--- service/Chatwork.php
+++ service/Chatwork.php
@@ -34,7 +34,7 @@ class Chatwork
             Logging::write('----- HANDLE FOR PAYLOAD -----');
             $message = $data['webhook_event']['body'];
             $data_webhook = $data['webhook_event'];
-            $reply_message = "[Reply aid={$data_webhook['account_id']} to={$data_webhook['room_id']}-{$data_webhook['message_id']}]";
+            $reply_message = "[rp aid={$data_webhook['account_id']} to={$data_webhook['room_id']}-{$data_webhook['message_id']}]";
             $responseMessage = $reply_message;
         } else {
             Logging::write('----- HANDLE FOR MANUAL -----');
