f7cb8f9718a3aaee5577fde05bcc7004a9590e63
Võ Chí Long (Long Áo Dài) <87090422+longaodai@users.noreply.github.com>
2024-10-04 16:24:49 +0700

    Update Gemini.php

--- service/Gemini.php
+++ service/Gemini.php
@@ -20,6 +20,7 @@ class Gemini
         BASEURL = 'https://generativelanguage.googleapis.com/v1beta';
         $text = filter_var($this->message, FILTER_SANITIZE_STRING);
         $url = sprintf("%s/models/%s:generateContent?key=%s", BASEURL, MODEL, $this->apiKey);
+        Logging::write("----- xxxxxxx -----: " . $url);
         // $url = 'https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=' . $this->apiKey;
         // $data = array(
         //     'prompt' => [
