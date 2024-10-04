ba6f86f4a32e6fad167cc2b2d7c744c245b7b12b
Võ Chí Long (Long Áo Dài) <87090422+longaodai@users.noreply.github.com>
2024-10-04 16:05:40 +0700

    :rocket: update service Gemini

--- service/Gemini.php
+++ service/Gemini.php
@@ -15,12 +15,27 @@ class Gemini
 
     public function sendRequest($message)
     {
-        $url = 'https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=' . $this->apiKey;
-        $data = array(
-            'prompt' => [
-                'text' => $message
-            ],
-        );
+        MODEL = 'gemini-1.5-flash';
+        BASEURL = 'https://generativelanguage.googleapis.com/v1beta';
+        $text = filter_var($this->message, FILTER_SANITIZE_STRING);
+        $url = sprintf("%s/models/%s:generateContent?key=%s", BASEURL, MODEL, $this->apiKey);
+        // $url = 'https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=' . $this->apiKey;
+        // $data = array(
+        //     'prompt' => [
+        //         'text' => $message
+        //     ],
+        // );
+        $data = [
+            "contents" => [
+                [
+                    "role" => "user",
+                    "parts" => [
+                        ["text" => $text]
+                    ]
+                ]
+            ]
+        ];
+
         $headers = array(
             'Content-Type: application/json'
         );
